066b4c467125d72c905dd598a8bd896f6e67c66c
KostasKoyias <sdi1500071@di.uoa.gr>
2019-11-27 14:26:24 +0200

    Remove from vector & shrink to fit size if requested

--- vector_t/src/main/vector.c
+++ vector_t/src/main/vector.c
@@ -15,7 +15,7 @@ int vector_init(void *vector, size_t size,
 
     // meta-data
     v->next = 0;
-    v->nmemb = MIN_ELEMENTS;
+    v->capacity = MIN_ELEMENTS;
     v->size = size;
 
     // actual content
@@ -38,8 +38,8 @@ int vector_insert(void *vector, ...){
 
     // double vector capacity if full
     if(vector_isfull(vector)){
-        v->nmemb = v->nmemb * 2;
-        v->data = realloc(v->data, v->size * v->nmemb);
+        v->capacity = v->capacity * 2;
+        v->data = realloc(v->data, v->size * v->capacity);
     }
 
     // use the appropriate constructor to initialize the new item
@@ -47,6 +47,36 @@ int vector_insert(void *vector, ...){
     v->init(v->data + ((v->next++) * v->size), props);
     va_end(props);
     return 0;
+}
+
+int vector_remove(void *vector, const uint64_t index){
+    vector_t *v= vector;
+    assert(vector);
+
+    if(v->next <= index)
+        return -1;
+
+    // destroy element in the specified position
+    if(v->destroy)
+        v->destroy(v->data + index * v->size);
+
+    // shift all elements one position to the left
+    for(uint64_t i = index; i < v->next - 1; i++)
+        memcpy(v->data + i * v->size, v->data + (i+1) * v->size, v->size);
+    v->next--;
+
+    return 0;
+}
+
+int vector_delete(void *vector, const void *key){
+    uint64_t pos; 
+    assert(vector && key);
+
+    // get position of the first matching element
+    if(vector_seek(vector, key, &pos) == NULL) 
+        return -1;
+    
+    return vector_remove(vector, pos);
 }
 
 void *vector_get(void *vector, uint64_t index){
@@ -58,15 +88,20 @@ void *vector_get(void *vector, uint64_t index){
     return v->data + index * v->size;
 }
 
-void *vector_seek(void *vector, const void *key){
+void *vector_seek(void *vector, const void *key, uint64_t *pos){
     vector_t *v = vector;
     void *item;
     assert(vector && key && v->seek);
 
     for(int i = 0; i < v->next; i++){
         item = v->data + i * v->size;
-        if(v->seek(item, key))
+        if(v->seek(item, key)){
+
+            // store position of the matching item & return a pointer to it
+            if(pos != NULL)
+                *pos = i;
             return item;
+        }
     }
 
     return NULL;
@@ -78,13 +113,23 @@ void vector_print(void *vector){
 
     // display meta-data
     fprintf(stdout, "\e[1;4mVector\e[0m\n * type size:\t%ld bytes\n * next:\t%lu\n * capacity:\t%lu\n * content\n",
-            v->size, v->next, v->nmemb);
+            v->size, v->next, v->capacity);
 
     // use the appropriate print function to showcase each item
     if(v->print)
         vector_foreach(v, v->print);
     else
         fprintf(stdout, "No printing method provided for this data type\n");
+}
+
+void vector_shrink(void *vector){
+    vector_t *v = vector;
+    assert(vector);
+
+    if(!vector_isfull(vector)){
+        v->data = realloc(v->data, v->next * v->size);
+        v->capacity = v->next;
+    }
 }
 
 // destroy a vector, letting go of all resources allocated
@@ -97,11 +142,11 @@ void vector_free(void *vector){
         vector_foreach(vector, v->destroy);
 
     reset(v->data);
-    v->next = v->nmemb = v->size = 0;
+    v->next = v->capacity = v->size = 0;
 }
 
 int vector_isfull(const void *vector){
     const vector_t *v = vector;
     assert(vector);
-    return v->next == v->nmemb;
+    return v->next == v->capacity;
 }
